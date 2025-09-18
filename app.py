abe7c88a812d5f7704b12258f70e1584ed4da8d5
Momodev <devmomo111@gmail.com>
2025-09-18 18:41:42 +0000

    Déploiement Render automatisé

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Mahmood! 🚀 Your Flask API is enjoying on Render..."
+    return "Hello, Mahmood! 🚀 Your Flask API is going well on Render..."
 
 if __name__ == "__main__":
     import os
